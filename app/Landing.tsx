3a987129cd7bea68ac616749a22e9b69e1961d04
Alfonzo <thulanikgakole@gmail.com>
2025-03-13 16:49:22 +0200

    starting with the landing page

---
+++ app/Landing.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Landing = () => {
+    const imagList = [
+        require('./../assets/images/1.jpg'),
+        require('./../assets/images/c1.png'),
+        require('./../assets/images/2.jpg'),
+        require('./../assets/images/c2.jpg'),
+        require('./../assets/images/3.jpg'),
+        require('./../assets/images/c1.jpg'),
+        require('./../assets/images/4.jpg'),
+        require('./../assets/images/6.jpg'),
+        require('./../assets/images/5.jpg'),
+        require('./../assets/images/1.jpg'),
+    ]
+  return (
+    <View>
+      <Text>Landing</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default Landing
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font'
 
 export default function RootLayout() {
-  return <Stack />;
+  
+const [ loaded, error ] = useFonts({
+  'outfit-r': require("./../assets/fonts/Outfit-Regular.ttf"),
+  'outfit-b': require("./../assets/fonts/Outfit-Bold.ttf")
+})
+
+  return (
+    <Stack>
+      <Stack.Screen 
+        name='Landing' 
+        options={{
+          headerShown: false
+        }}
+      />
+    </Stack>
+  )
 }
